cabb1117d13fd191258ea585c6044f00a227ba2f
ruthjoy76 <ruthjoy76@gmail.com>
2024-02-14 16:08:08 +0800

    New Lesson

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-let sales = 123_456_789;
-let course = "TypeScript";
-let is_published = true;
-let level;
+// let sales = 123_456_789;
+// let course = "TypeScript";
+// let is_published = true;
+// let level;
+
+// let user: [number, string, boolean, number] = [1, 'Mosh', true, 0];
+// user.push(1);
+
+//const small = 1;
+//const medium = 2;
+//const large = 3;
+
+// const enum Size {Small = 1, Medium, Large};
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022)
+     return income * 1.2;
+   return income * 1.3;
+}
+
+calculateTax(10_000);
