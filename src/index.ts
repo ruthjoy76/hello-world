383a0f953d827cdc6f865c28e28a332ba80d62cb
ruthjoy76 <ruthjoy76@gmail.com>
2024-02-19 20:09:03 +0800

    New Lesson

--- src/index.ts
+++ src/index.ts
@@ -47,3 +47,19 @@
 
 // kgToLbs(10);
 // kgToLbs('10kg');
+
+type Draggable = {
+    drag: () => void
+};
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox:UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
